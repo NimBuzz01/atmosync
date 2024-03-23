044916b6e846deaa5f5a3f476cf16afb7c812c08
niamatmarjan <86093827+NimBuzz01@users.noreply.github.com>
2024-03-23 14:12:05 +0530

    add spotify api

--- client/src/components/dashboard/player-container.tsx
+++ client/src/components/dashboard/player-container.tsx
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { WebcamTest } from "../info-container/webcam-sample-test";
+import { getMusicByGenre } from "@/lib/utils/spotify";
 
 const PlayerContainer = () => {
+  const [music, setMusic] = React.useState([]);
+  useEffect(() => {
+    getMusicByGenre("classical");
+    setMusic(music);
+  }, []);
   return <div className="h-full p-4 bg-slate-700 text-slate-100"></div>;
 };
 
